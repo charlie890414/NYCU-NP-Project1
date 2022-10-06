6a7a4a9bd512c9cf08445119b09765fc8188f144
charlie890414 <charlie890414@gmail.com>
2022-10-07 03:44:20 +0800

    try to add pipe in code

--- npshell.cpp
+++ npshell.cpp
@@ -8,6 +8,34 @@ using namespace std;
 
 map<string, string> envp;
 
+void trimRight(string &str,
+               const string &trimChars = " ")
+{
+    string::size_type pos = str.find_last_not_of(trimChars);
+    str.erase(pos + 1);
+}
+
+void trimLeft(string &str,
+              const string &trimChars = " ")
+{
+    string::size_type pos = str.find_first_not_of(trimChars);
+    str.erase(0, pos);
+}
+
+void trim(string &str, const string &trimChars = " ")
+{
+    trimRight(str, trimChars);
+    trimLeft(str, trimChars);
+}
+
+vector<smatch> matchStr(string str, string matcher)
+{
+    regex matcher_regex(matcher);
+    sregex_iterator iter(str.begin(), str.end(), matcher_regex);
+    sregex_iterator end;
+    return {iter, end};
+}
+
 vector<string> splitStr(string str, string delimiter)
 {
     regex delimiter_regex(delimiter);
@@ -29,7 +57,7 @@ void setenv(string var, string value)
 
 void printenv(string var)
 {
-    if(envp.contains(var))
+    if (envp.contains(var))
         cout << envp[var] << endl;
 }
 
@@ -39,24 +67,46 @@ void exit()
 }
 
 // helper function
-void printIter(auto iter)
+void printIter(vector<string> iter)
 {
     for (auto i : iter)
         cout << i << " ";
     cout << endl;
 }
 
+// helper function
+void printIter(vector<smatch> iter)
+{
+    for (auto i : iter)
+        cout << i.str() << " ";
+    cout << endl;
+}
+
 int main()
 {
     envp.insert(pair<string, string>("PATH", "bin:."));
-    string cmdStr;
-    while (getCommand(cmdStr))
+    string cmdsStr;
+    while (getCommand(cmdsStr))
     {
-        vector<string> cmd = splitStr(cmdStr, "\\s+");
-        // printIter(cmd);
-        if (cmd[0] == "exit") exit();
-        else if(cmd[0] == "setenv") setenv(cmd[1], cmd[2]);
-        else if(cmd[0] == "printenv") printenv(cmd[1]);
+        vector<string> cmds = splitStr(cmdsStr, "\\s+\\|\\s+");
+        vector<smatch> pipes = matchStr(cmdsStr, "\\|");
+        printIter(cmds);
+        printIter(pipes);
+        for (string cmdStr : cmds)
+        {
+            vector<string> cmd = splitStr(cmdStr, "\\s+");
+            printIter(cmd);
+            if (cmd[0] == "exit")
+                exit();
+            else if (cmd[0] == "setenv")
+                setenv(cmd[1], cmd[2]);
+            else if (cmd[0] == "printenv")
+                printenv(cmd[1]);
+            else
+            {
+                cerr << "Unknown command: [" << cmd[0] << "]." << endl;
+            }
+        }
     }
     cout << endl;
     return 0;
